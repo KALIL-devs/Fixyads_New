c42c6a9aee79ba32ba2408f8e550aa0774ff179b
KALIL-devs <kalilrahman055@gmail.com>
2026-01-25 21:32:51 +0530

    client list

--- src/components/ClientsSection/ClientsSection.tsx
+++ src/components/ClientsSection/ClientsSection.tsx
@@ -8,48 +8,63 @@ type Client = {
 };
 
 const clients: Client[] = [
-  {
-    name: "Tahrshop",
-    logo: "/Clients/Tahrshop.png",
-  },
-  {
-    name: "Nexvyon",
-    logo: "/Clients/Nexvyon.png",
-  },
-  {
-    name: "Charvi Boutique",
-    logo: "/Clients/logo.png",
-  },
+  { name: "Tahrshop", logo: "/Clients/Tahrshop.png" },
+  { name: "Nexvyon", logo: "/Clients/Nexvyon.png" },
+  { name: "Charvi Boutique", logo: "/Clients/logo.png" },
   {
     name: "Koothan Infotech",
     logo: "/Clients/koothan.png",
-    wide: true, // wide text-based logo
+    wide: true,
   },
 ];
 
 export default function ClientsSection() {
   return (
     <section className={styles.clientsSection}>
-      <h2 className={styles.title}>Our Clients</h2>
+      <h2 className={styles.title}>Trusted by Our Clients</h2>
+        <div className={styles.clientsGrid}>
+          {clients.map((client) => (
+            <div>
+              <div
+                key={client.name}
+                className={`${styles.clientLogo} ${
+                  client.wide ? styles.wideLogo : ""
+                }`}
+              >
+                <Image
+                  src={client.logo}
+                  alt={client.name}
+                  width={180}
+                  height={90}
+                  className={styles.logo}
+                />
+              </div>
+            </div>
 
-      <div className={styles.clientsGrid}>
-        {clients.map((client) => (
-          <div
-            key={client.name}
-            className={`${styles.clientLogo} ${
-              client.wide ? styles.wideLogo : ""
-            }`}
-          >
-            <Image
-              src={client.logo}
-              alt={client.name}
-              width={160}
-              height={80}
-              className={styles.logo}
-            />
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+        <div className={styles.clientsGrid}>
+          {clients.map((client) => (
+            <div>
+              <div
+                key={client.name}
+                // className={`${styles.clientLogo} ${
+                //   client.wide ? styles.wideLogo : ""
+                // }`}
+              >
+                <h4 className={styles.clientname}>{client.name}</h4>
+                {/* <Image
+                  src={client.logo}
+                  alt={client.name}
+                  width={180}
+                  height={90}
+                  className={styles.logo}
+                /> */}
+              </div>
+            </div>
+
+          ))}
+        </div>
     </section>
   );
 }
