89ea5962b9a89416c997a60730d630fa1a22759b
KALIL-devs <kalilrahman055@gmail.com>
2026-01-27 12:49:16 +0530

    schema added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import Footer from "@/components/Footer/Footer";
 import FloatingQuote from "@/components/FloatingQuote/FloatingQuote";
 import "./globals.css";
 import Script from "next/script";
+import { FAQPage, WithContext } from "schema-dts";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,6 +50,42 @@ export default function RootLayout({
             gtag('config', 'G-0GMKDXQV76');
           `}
         </Script>
+
+        <Script id="article-schema" type="application/ld+json" strategy="afterInteractive">
+          {JSON.stringify({
+            "@context": "https://schema.org/",
+            "@type": "Article",
+            "mainEntityOfPage": {
+              "@type": "WebPage",
+              "@id": "https://www.fixyads.com"
+            },
+            "headline": "Fixyads - Digital Marketing Agency and Training Institute",
+            "description": "Expert Digital Marketing, Branding, and Social Media Marketing solutions built for measurable growth.",
+            "image": {
+              "@type": "ImageObject",
+              "url": "aboutus/aboutus.png", // add your image URL
+              // "width": "",
+              // "height": ""
+            },
+            "author": {
+              "@type": "Organization",
+              "name": "Fixyads"
+            },
+            "publisher": {
+              "@type": "Organization",
+              "name": "Fixyads",
+              "logo": {
+                "@type": "ImageObject",
+                "url": "https://www.fixyads.com/", // replace with actual logo URL
+                // "width": "",
+                // "height": ""
+              }
+            },
+            "datePublished": "2026-01-01",
+            "dateModified": "2026-01-01"
+          })}
+        </Script>
+
 
         <Navbar />
         <main style={{ minHeight: "calc(100vh - 400px)" }}>
