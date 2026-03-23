b961a2512a64d8c3db3d6e25e84c21d966b57ef2
KALIL-devs <kalilrahman055@gmail.com>
2026-03-24 00:06:45 +0530

    i think this will be final

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -112,7 +112,7 @@ export default function AboutPage() {
           <div className={styles.teamGrid_1}>
             {[
               { name: 'Gowtham', role: 'Social Media Expert', img: '/Team/gowtham.png' },
-              { name: 'Yoga', role: 'Social Media Expert', img: '/Team/yoga.png' },
+              // { name: 'Yoga', role: 'Social Media Expert', img: '/Team/yoga.png' },
               { name: 'Yoga', role: 'Social Media Expert', img: '/Team/yoga.png' },
               { name: 'Aruna', role: 'Digital Marketing Specialist', img: '/Team/aruna.png' },
             ].map((member) => (
